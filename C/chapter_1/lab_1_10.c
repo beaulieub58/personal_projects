266e4544db781a184df1bc8b641fc56ecb3cadd0
Ben Beaulieu <beaulieub58@gmail.com>
2024-09-06 19:26:26 -0400

    programming assignment 1

---
+++ C/chapter_1/lab_1_10.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void){
+
+
+    int userInput = ' ';
+    printf("Enter Input Here: ");
+    scanf("%d",&userInput);
+    getchar();
+
+
+    printf("\n");
+    printf("   * \n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf("  ***\n");
+
+    printf("\n");
+
+    printf("^   ^\n");
+    printf(" o o \n");
+    printf("=   =\n");
+    printf(" ---\n");
+
+    printf("\n");
+
+    printf("  %d \n",userInput);
+    printf(" %d%d%d\n",userInput,userInput,userInput);
+    printf("%d%d%d%d%d\n",userInput,userInput,userInput,userInput,userInput);
+
+    printf("\n");
+
+
+    getchar();
+
+}
