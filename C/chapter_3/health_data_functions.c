56e2b89dc6255492519b8f35273c3f19061dff04
Ben Beaulieu <beaulieub58@gmail.com>
2024-09-18 22:03:04 -0400

    updates to homework file

--- C/chapter_3/health_data_functions.c
+++ C/chapter_3/health_data_functions.c
@@ -31,7 +31,7 @@ int divisionFunction(int given, int divisor){
 int main(void){
 
     int userAgeYears;
-    int userAgeDays;
+    //int userAgeDays;
     //int userAgeMinutes;
     //int userAgeSeconds;
     //int totalHeartbeats;
@@ -47,34 +47,22 @@ int main(void){
     printf("\n");
 
     printf("You are approximately %d days old.\n", multiplicationFunction(userAgeYears, DAYS_IN_YEARS));
-   
-    userAgeDays = multiplicationFunction(userAgeYears, DAYS_IN_YEARS) + divisionFunction(userAgeYears, LEAP_YEAR_INPUT); // Add days for leap years
-   
-    //userAgeMinutes = userAgeDays * HOURS_IN_DAYS * MINUTES_IN_HOUR;   // 24 hours/day, 60 minutes/hour
-    printf("You are approximately %d minutes old.\n", multiplicationFunction(multiplicationFunction(userAgeDays, HOURS_IN_DAYS), MINUTES_IN_HOUR));   
 
-    //userAgeSeconds = userAgeMinutes * SECONDS_IN_MINUTES;
-    printf("You are approximately %d seconds old.\n", multiplicationFunction(multiplicationFunction(multiplicationFunction(userAgeDays, HOURS_IN_DAYS), MINUTES_IN_HOUR), SECONDS_IN_MINUTES));
+    printf("You are approximately %d minutes old.\n", multiplicationFunction(multiplicationFunction(multiplicationFunction(userAgeYears, DAYS_IN_YEARS) + divisionFunction(userAgeYears, LEAP_YEAR_INPUT), HOURS_IN_DAYS), MINUTES_IN_HOUR));   
 
-    //totalHeartbeats = userAgeMinutes * AVG_BEATS_PER_MINUTE;
-    printf("Your heart has beat approximately %d times.\n", multiplicationFunction(multiplicationFunction(multiplicationFunction(userAgeDays, HOURS_IN_DAYS), MINUTES_IN_HOUR), AVG_BEATS_PER_MINUTE));
+    printf("You are approximately %d seconds old.\n", multiplicationFunction(multiplicationFunction(multiplicationFunction(multiplicationFunction(userAgeYears, DAYS_IN_YEARS) + divisionFunction(userAgeYears, LEAP_YEAR_INPUT), HOURS_IN_DAYS), MINUTES_IN_HOUR), SECONDS_IN_MINUTES));
 
-    //totalSneezes = userAgeDays * SNEEZES_PER_DAY;
-    printf("You have sneezed approximately %d times.\n", multiplicationFunction(userAgeDays, SNEEZES_PER_DAY));
+    printf("Your heart has beat approximately %d times.\n", multiplicationFunction(multiplicationFunction(multiplicationFunction(multiplicationFunction(userAgeYears, DAYS_IN_YEARS) + divisionFunction(userAgeYears, LEAP_YEAR_INPUT), HOURS_IN_DAYS), MINUTES_IN_HOUR), AVG_BEATS_PER_MINUTE));
 
-    //totalCaloriesBurned = userAgeDays * CALORIES_BURNED_PER_DAY;
-    printf("You have burned approximately %d calories.\n", multiplicationFunction(userAgeDays, CALORIES_BURNED_PER_DAY));
+    printf("You have sneezed approximately %d times.\n", multiplicationFunction(multiplicationFunction(userAgeYears, DAYS_IN_YEARS) + divisionFunction(userAgeYears, LEAP_YEAR_INPUT), SNEEZES_PER_DAY));
 
-    //totalCheesePizzasEaten = totalCaloriesBurned / CALORIES_IN_CHEESE_PIZZA;
-    printf("The number of calories burned throughout your lifetime equates to approximately %d cheese pizzas.\n", divisionFunction(multiplicationFunction(userAgeDays, CALORIES_BURNED_PER_DAY), CALORIES_IN_CHEESE_PIZZA));
+    printf("You have burned approximately %d calories.\n", multiplicationFunction(multiplicationFunction(userAgeYears, DAYS_IN_YEARS) + divisionFunction(userAgeYears, LEAP_YEAR_INPUT), CALORIES_BURNED_PER_DAY));
 
-    //breathesPerMinute = BREATHES_PER_HOUR / MINUTES_IN_HOUR;
-    //breathesPerMinute = divisionFunction(BREATHES_PER_HOUR, MINUTES_IN_HOUR);
+    printf("The number of calories burned throughout your lifetime equates to approximately %d cheese pizzas.\n", divisionFunction(multiplicationFunction(multiplicationFunction(userAgeYears, DAYS_IN_YEARS) + divisionFunction(userAgeYears, LEAP_YEAR_INPUT), CALORIES_BURNED_PER_DAY), CALORIES_IN_CHEESE_PIZZA));
 
-    //totalBreathesTaken = breathesPerMinute * userAgeMinutes;
-    printf("The number of breaths you've taken throughout your lifetime is approximately %d.\n",multiplicationFunction(divisionFunction(BREATHES_PER_HOUR, MINUTES_IN_HOUR),multiplicationFunction(multiplicationFunction(userAgeDays, HOURS_IN_DAYS), MINUTES_IN_HOUR)));
+    printf("The number of breaths you've taken throughout your lifetime is approximately %d.\n",multiplicationFunction(divisionFunction(BREATHES_PER_HOUR, MINUTES_IN_HOUR),multiplicationFunction(multiplicationFunction(multiplicationFunction(userAgeYears, DAYS_IN_YEARS) + divisionFunction(userAgeYears, LEAP_YEAR_INPUT), HOURS_IN_DAYS), MINUTES_IN_HOUR)));
 
-    printf("\n");
     getchar();
+    
     return 0;
 }
