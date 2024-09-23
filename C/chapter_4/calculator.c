fb3145300ddf57e7852d638f720561949f3a6789
Ben Beaulieu <beaulieub58@gmail.com>
2024-09-22 22:35:36 -0400

    first iteration of chapter 4 homework

---
+++ C/chapter_4/calculator.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+double division(double dividend, double divisor ) {
+
+    return dividend / divisor;
+    
+}
+
+double multiplication(double multiple_1, double multiple_2) {
+
+    return multiple_1 * multiple_2;
+
+}
+
+double addition(double add_1, double add_2) {
+
+    return add_1 + add_2;
+
+}
+
+double subtraction(double starter, double subtractor){
+
+    return starter - subtractor;
+}
+
+int main(void) {
+
+    int selection;
+    double input_1;
+    double input_2;
+    double output;
+
+    printf("Select Number:\n 1. For Addition\n2. For Subtraction\n3. For Multiplication\n 4. For Division\n");
+    scanf("%d", &selection);
+    printf("Enter your first input:\n");
+    scanf("%d", &input_1);
+    printf("Enter your second input:\n");
+    scanf("%d", &input_2);
+
+    if (selection == 1) {
+        
+        output = addition(input_1, input_2);
+
+    }
+
+    else if (selection == 2) {
+
+        output = subtraction(input_1, input_2);
+
+    }
+
+    else if (selection == 3) {
+
+        output = multiplication(input_1, input_2);
+
+
+    }
+
+    else {
+
+        output = division(input_1, input_2);
+    }
+
+
+
+}
