bb4ea24983a4c1014764312eb5e4b47334be3105
Ben Beaulieu <beaulieub58@gmail.com>
2024-11-14 22:01:46 -0500

    practice

---
+++ Java/test_files/personal_information.java
@@ -0,0 +1,22 @@
+package Java.test_files;
+public class personal_information {
+
+    //use system.out.println to print hardcoded values//
+
+    
+    public static void main(String[] args) {
+        
+
+        //print name
+        System.out.println("Benjamin Beaulieu");
+        //print address with street, city, state, and zip
+        System.out.println("32 Donovan Drive, Quinebaug, Connecticut, 06262");
+        //print a fake telephone number
+        System.out.println("508-678-5942");
+        //print my college major
+        System.out.println("I am majoring in: Undeclared");
+
+    }
+
+    
+}
